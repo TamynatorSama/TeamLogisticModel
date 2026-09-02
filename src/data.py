619052433ac69894d4698d622ceb2267bc10f2a4
SWerner26 <swerner2263@gmail.com>
2026-09-02 18:37:34 +0000

    Add data loading function

--- src/data.py
+++ src/data.py
@@ -4,7 +4,3 @@ def load_data(filename):
     """Load observation data from a CSV file."""
     data = pd.read_csv(filename)
     return data
-
-if __name__ == "__main__":
-    data = load_data("data/observations.csv")
-    print(data.head())
